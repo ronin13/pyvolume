3c79cdb90528645de522799682ac2874b09a07c0
Raghavendra Prabhu <me@rdprabhu.com>
2016-11-08 07:59:13 +0000

    Add more tests

--- tests/test_pyvolume_sshfs.py
+++ tests/test_pyvolume_sshfs.py
@@ -10,10 +10,13 @@ Tests for `pyvolume` module for sshfs implementation.
 
 import pytest
 import unittest
+import os
 
 from flask import jsonify
 import json
+from mock import patch
 import pyvolume.manager as manager
+from pyvolume.exceptions import NeedOptionsException
 
 # List of endpoints to be tested.
 E_IMPLEMENTS='/Plugin.Activate'
@@ -39,7 +42,7 @@ class ManagerTestCase(unittest.TestCase):
 
     def setUp(self):
         manager.app.config['TESTING'] = True
-        manager.app.config['vmanager'] = volmer
+        manager.app.config['volmer'] = volmer
         self.app = manager.app.test_client()
 
     def test_get(self):
@@ -51,6 +54,26 @@ class ManagerTestCase(unittest.TestCase):
         rv = self.app.post(E_IMPLEMENTS)
         self.assertEqual({"Implements": ["VolumeDriver"]}, json.loads(rv.data))
         self.assertEqual(rv.status_code, 200)
+
+    def test_create(self):
+        with patch.object(os, 'mkdir', return_value=None) as mock_method:
+            rv = self.app.post(E_CREATE, data=json.dumps({
+                "Name": "TESTVOL",
+                "Opts": {
+                            "remote_path": "server:/home/user",
+                         },
+                }), content_type='application/json')
+            self.assertEqual({"Err": ""}, json.loads(rv.data))
+            self.assertEqual(rv.status_code, 200)
+
+            rv = self.app.post(E_CREATE, data=json.dumps({
+                "Name": "TESTVOL",
+                "Opts": {},
+                }), content_type='application/json')
+            self.assertEqual({"Err": "Failed to create the volume TESTVOL : remote_path is a required option for sshfs"},
+                    json.loads(rv.data))
+            self.assertEqual(rv.status_code, 400)
+
 
     def test_shutdown(self):
         with pytest.raises(RuntimeError):
